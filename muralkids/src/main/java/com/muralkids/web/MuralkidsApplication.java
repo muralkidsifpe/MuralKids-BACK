d19d6d945d49ee056ee8b8000a4a9f0927bbcb05
Luiz Carlos <lc.filho@gmail.com>
2019-11-24 13:25:31 -0300

    Criação do projeto em Spring Boot MVC - estrutura inicial

---
+++ muralkids/src/main/java/com/muralkids/web/MuralkidsApplication.java
@@ -0,0 +1,13 @@
+package com.muralkids.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuralkidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuralkidsApplication.class, args);
+	}
+
+}
